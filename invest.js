47fb6da6e8d1cfb991c84155af5ba4aa3c476fd9
Martynas Miliauskas <martin.miliauskas@gmail.com>
2018-02-09 22:01:37 +0200

    start using new crowdsale contract

--- invest.js
+++ invest.js
@@ -1,25 +1,20 @@
 var Web3 = require('web3');
 var config = require('config');
-var express = require("express");
 
 var ConectumICO = require('./contracts/ConectumICO.json');
 
 var ethNodeAddress = config.get('eth-node-address');
+var owner = config.get('owner');
 var icoContractAddress = config.get('ico-contract-address');
 var web3 = new Web3(Web3.givenProvider || ethNodeAddress);
 var icoContract = new web3.eth.Contract(ConectumICO.abi, icoContractAddress);
 
-var app = express();
-
-app.get("/", (req, res) => {
-    let stats = {};
-    icoContract.methods.weiRaised().call()
-        .then((weiRaised) => {
-            stats.weiRaised = weiRaised;
-            res.json(stats);
-        });
-});
-
-var port = 9000;
-app.listen(port);
-console.log(`Listening on port ${port}`);
+web3
+    .eth
+    .sendTransaction({
+        to: icoContractAddress, 
+        from: owner, 
+        value: web3.utils.toWei('1', 'ether')
+    })
+    .then(console.log)
+    .catch(console.log);
