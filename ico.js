b451993af200debce57862a623cbe87a35e44cb9
marti-1 <martin.miliauskas@gmail.com>
2018-02-26 21:52:29 +0200

    refactor

--- ico.js
+++ ico.js
@@ -14,38 +14,35 @@ var web3 = new Web3(Web3.givenProvider || ethNodeAddress);
 var icoContract = new web3.eth.Contract(ConectumICO.abi, icoContractAddress);
 var tokenContract = new web3.eth.Contract(COMToken.abi, tokenContractAddress);
 
-function setReference(participant, referrer) {
-    web3.eth.personal.unlockAccount(owner,ownerPassword).then((response) => {
-        icoContract.methods.setReference(participant, referrer).send({from: owner}).then(console.log).catch(console.log);
-    }).catch(console.log);
+Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send;
+
+// Increases testrpc time by the passed duration in seconds
+function increaseTime (duration) {
+  const id = Date.now();
+
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: '2.0',
+      method: 'evm_increaseTime',
+      params: [duration],
+      id: id,
+    }, err1 => {
+      if (err1) return reject(err1);
+
+      web3.currentProvider.sendAsync({
+        jsonrpc: '2.0',
+        method: 'evm_mine',
+        id: id + 1,
+      }, (err2, res) => {
+        return err2 ? reject(err2) : resolve(res);
+      });
+    });
+  });
 }
 
-function getBalanceOf(acc) {
-    tokenContract.methods.balanceOf(acc).call().then(console.log).catch(console.log);
-}
-
-function finalize() {
-    web3.eth.personal.unlockAccount(owner, ownerPassword).then((response) => {
-        icoContract.methods.finalize().send({from: owner}).then(console.log).catch(console.log);
-    }).catch(console.log);
-}
-
-function releaseVestedFor(acc) {
-    web3.eth.personal.unlockAccount(owner, ownerPassword).then((response) => {
-        tokenContract.methods.releaseVestedFor(acc).send({from: owner}).then(console.log).catch(console.log);
-    }).catch(console.log);
-}
-
-function unclockFor(acc) {
-    web3.eth.personal.unlockAccount(owner, ownerPassword).then((response) => {
-        tokenContract.methods.releaseTimelockedFor(acc).send({from: owner}).then(console.log).catch(console.log);
-    }).catch(console.log);
-}
-
-function incStage() {
-    web3.eth.personal.unlockAccount(owner, ownerPassword).then((response) => {
-        icoContract.methods.incStage().send({from: owner}).then(console.log).catch(console.log);
-    }).catch(console.log);
+function invest(from, amount) {
+    const ethAmount = web3.utils.toWei(amount.toString(), 'ether');
+    return web3.eth.sendTransaction({to: icoContractAddress, value: ethAmount, from: from});
 }
 
 const commandLineArgs = require('command-line-args')
@@ -57,7 +54,7 @@ const optionDefinitions = [
         description: 'Set reference'
     },
     {
-        name: 'getBalanceOf',
+        name: 'balanceOf',
         type: String,
         description: 'Get COM token balance'
     },
@@ -71,41 +68,77 @@ const optionDefinitions = [
         type: String
     },
     {
-        name: 'unlockFor',
+        name: 'releaseTimelockedFor',
         description: 'Unlock timelocked tokens for a user',
         type: String
     },
     {
         name: 'incStage',
         description: 'Increase ICO stage'
+    },
+    {
+        name: 'incTime',
+        description: 'Increase block time (works only for the testrpc)'
+    },
+    {
+        name: 'currentTime',
+        description: 'Get current block timestamp'
+    },
+    {
+        name: 'invest',
+        description: 'Invest ether',
+        multiple: true
+    },
+    {
+        name: 'isActive',
+        description: 'Is ICO in active stage'
     }
 ];
 
-const options = commandLineArgs(optionDefinitions)
-
-switch (Object.keys(options)[0]) {
-    case "setReference": {
-        setReference(options.setReference[0], options.setReference[1]);
-        break;
-    }
-    case "getBalanceOf": {
-        getBalanceOf(options.getBalanceOf);
-        break;
-    }
-    case "finalize": {
-        finalize();
-        break;
-    }
-    case "releaseVestedFor": {
-        releaseVestedFor(options.releaseVestedFor[0]);
-        break;
-    }
-    case "unclockFor": {
-        unlockFor(options.unclockFor[0]);
-        break;
-    }
-    case "incStage": {
-        incStage();
-        break;
+function sendICO(method, params) {
+    if (process.env.NODE_ENV == 'production') {
+        return web3.eth.personal.unlockAccount(owner, ownerPassword)
+            .then(icoContract.methods[method].apply(null, params).send({from: owner}))
+    } else {
+        return icoContract.methods[method].apply(null, params).send({from: owner});
     }
 }
+
+function sendToken(method, params) {
+    if (process.env.NODE_ENV == 'production') {
+        return web3.eth.personal.unlockAccount(owner, ownerPassword)
+            .then(tokenContract.methods[method].apply(null, params).send({from: owner}))
+    } else {
+        return tokenContract.methods[method].apply(null, params).send({from: owner});
+    }
+}
+
+function callICO(method, params) {
+    return icoContract.methods[method].apply(null, params).call();
+}
+
+function callToken(method, params) {
+    return tokenContract.methods[method].apply(null, params).call();
+}
+
+function executeMethod(method, params, methods) {
+    const m = methods[method];
+    return m(params);
+}
+
+const methods = {
+    setReference: (p) => sendICO('setReference', p),
+    balanceOf: (p) => callToken('balanceOf', [p]),
+    finalize: (p) => sendICO('finalize'),
+    releaseVestedFor: (p) => sendToken('releaseVestedFor', [p]),
+    releaseTimelockedFor: (p) => sendToken('releaseTimelockedFor', [p]),
+    incStage: (p) => sendICO('incStage', p),
+    incTime: (p) => increaseTime(p),
+    invest: (p) => invest.apply(null, p),
+    isActive: (p) => callICO('isActive', p)
+};
+
+const options = commandLineArgs(optionDefinitions);
+const methodName = Object.keys(options)[0];
+const params = options[methodName] || [];
+executeMethod(methodName, params, methods).then(console.log).catch(console.log);
